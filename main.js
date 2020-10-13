3ee883d26645e6241070053b0bc42b962cb72ea0
Tiago Santos <tiagosantosdev@hotmail.com>
2020-10-13 14:37:41 -0300

    Typescript

---
+++ main.js
@@ -0,0 +1,12 @@
+var minhaVr = "MInha variavel";
+function minhaFnc(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var lista_numeros = ["Angular", "Typescript"];
+var PrimeiraClasse = /** @class */ (function () {
+    function PrimeiraClasse() {
+    }
+    return PrimeiraClasse;
+}());
---
+++ main.ts
@@ -0,0 +1,18 @@
+var minhaVr: string = "MInha variavel"
+
+function minhaFnc(x, y){
+    return x + y;
+}
+
+let num = 2
+
+const PI = 3.14;
+
+var lista_numeros =["Angular", "Typescript"]
+
+
+class PrimeiraClasse{
+        constructor(){
+            
+        }
+}
